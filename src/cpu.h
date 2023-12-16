82bfb7c03990a2d8b3208a44648e1eb2b3fe3b1c
Eric Chang <eric141822@gmail.com>
2023-12-16 13:41:13 -0600

    list of instructions, and CPU_FLAGs

--- src/cpu.h
+++ src/cpu.h
@@ -17,16 +17,18 @@ enum AddressingMode
     NoneAddressing,
 };
 
-struct CPU_flags
+const extern uint16_t STACK_T = 0x0100;
+const extern uint16_t STACK_RESET = 0xfd;
+struct CPU_FLAGS
 {
-    const uint8_t CARRY = 0b00000001;
-    const uint8_t ZERO = 0b00000010;
-    const uint8_t INTERRUPT = 0b00000100;
-    const uint8_t DECIMAL_UNUSED = 0b00001000;
-    const uint8_t BREAK = 0b00010000;
-    const uint8_t BREAK2 = 0b00100000;
-    const uint8_t OVERFLOW = 0b01000000;
-    const uint8_t NEGATIVE = 0b10000000;
+    static constexpr uint8_t CARRY = 0b00000001;
+    static constexpr uint8_t ZERO = 0b00000010;
+    static constexpr uint8_t INTERRUPT = 0b00000100;
+    static constexpr uint8_t DECIMAL_UNUSED = 0b00001000;
+    static constexpr uint8_t BREAK = 0b00010000;
+    static constexpr uint8_t BREAK2 = 0b00100000;
+    static constexpr uint8_t OVERFLOW = 0b01000000;
+    static constexpr uint8_t NEGATIVE = 0b10000000;
 };
 struct CPU
 {
@@ -46,11 +48,59 @@ struct CPU
     void load_and_run(std::vector<uint8_t> program);
     void load(std::vector<uint8_t> program);
     void run();
+
+    /* ------ HELPERS ------ */
     void set_zero_and_negative_flags(uint8_t register_value);
+    bool check_status_flag();
+    /* --------------------- */
     void lda(AddressingMode mode);
     void tax();
     void inx();
     void sta(AddressingMode mode);
+
+    /* ------ TODO ------ */
+    void adc(AddressingMode mode);
+    void sbc(AddressingMode mode);
+    void and_op(AddressingMode mode);
+    void asl(AddressingMode mode);
+    void branch(bool cond); // branch if cond is true
+    void clear(uint8_t flag_to_clear); // clear flag
+    void cmp_op(AddressingMode mode);
+    void cpx(AddressingMode mode);
+    void cpy(AddressingMode mode);
+    void dec(AddressingMode mode);
+    void dex(); // might not be needed, just implied op
+    void dey(); // might not be needed, just implied op
+    void eor(AddressingMode mode);
+    void inc(AddressingMode mode);
+    void iny();
+    void jmp(AddressingMode mode);
+    void jsr();
+    void ldx(AddressingMode mode);
+    void ldy(AddressingMode mode);
+    void lsr(AddressingMode mode);
+    void ora(AddressingMode mode);
+    void pha();
+    void php();
+    void pla();
+    void plp();
+    void rol(AddressingMode mode);
+    void ror(AddressingMode mode);
+    void rti();
+    void rts();
+    void sbc(AddressingMode mode);
+    void sec(); // implied
+    void sed(); // implied
+    void sei(); // implied
+    void stx(AddressingMode mode);
+    void sty(AddressingMode mode);
+    void tay();
+    void tsx();
+    void txa();
+    void txs();
+    void tya();
+
+
 
     uint16_t get_operand_address(AddressingMode mode);
 };
